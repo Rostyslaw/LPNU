ca5924ea12250262916f8edebbe459f18318661d
Ростислав <frek2816@gmail.com>
2017-12-09 02:51:15 +0200

    LR-4_Part-2

--- Lab_4_Agorithms/src/BFR.java
+++ Lab_4_Agorithms/src/BFR.java
@@ -1,3 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class BFR {
+
+    try{
+        File file = new File("BFR.in");
+        Scanner sc = new Scanner(file);
+    }
+    catch(FileNotFoundException e){
+
+    }
 
 }
---
+++ Lab_4_Agorithms/src/FWrite.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class FWrite {
+
+    public void write(){
+        File file = new File("BFR.in");
+    }
+
+
+
+}
