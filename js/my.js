3b1c54f9359b49c197afe4f2be7c84468b0ea754
Ростислав Фреїк <frek2816@gmail.com>
2018-01-09 12:07:37 +0200

    LR8

--- js/my.js
+++ js/my.js
@@ -1,36 +1,44 @@
+var i = 0;
 
- var newsButton = document.getElementById('sendArticle')
- var articleField = document.getElementById('article')
- var title = document.getElementById('title')
+function isOnline() {
+    return window.navigator.onLine;
+}
 
- newsButton.addEventListener('click', function() {
-     var articleFieldValue = articleField.value;
-     var titleFieldValue = title.value;
+function addNews() {
+	var name = document.getElementById('newsHeader').value;
+	var text = document.getElementById('newsText').value;
+	var newsPic	= document.getElementById('news-img').value;
 
-     if(articleFieldValue.length == 0 || titleFieldValue.length == 0){
-       window.alert('Будьласка заповніть усі поля');
-     }
-     else{
-       window.alert('Новину опубліковано');
-       articleField.value = ''
-       title.value = ''
-     }
-  });
+		if( name == "" || newsPic == ""	|| text == ""){
+				alert("Заповніть всі поля!");
+				return;
+			}
+		if (isOnline()) {
+			document.getElementById('newsHeader').value = '';
+			document.getElementById('newsText').value = '';
+			target.src = '../images/Random-turtle.jpg'
+			alert('Новина додана');
+		} else{
+			imgData = target.src;
+			i++;
+			var list = [];
+				 list.push({"name": (name),"text": (text)});
+			localStorage.setItem('n' + i, JSON.stringify(list));
+			localStorage.setItem('i' + i, (imgData));
+			document.getElementById('newsHeader').value = '';
+			document.getElementById('newsText').value = '';
+			target.src = '../images/Random-turtle.jpg'
+		}
+}
 
-
-
-/* var adminpanels = document.createElement('div');
-        adminpanels.classList.add('feedback');
-
-        var article = document.createElement('h2');
-        article.innerHTML = articleFieldValue
-
-        var titles = document.createElement('p');
-        titles.innerText =  titleFieldValue
-
-        adminpanels.appendChild(article);
-        adminpanels.appendChild(titles);
-        document.getElementById('newsWrap').appendChild(adminpanels);
-        articleField.value = ''
-        title.value = ''
-        */
+function showImage(src, target) {
+    var fr = new FileReader();
+    // when image is loaded, set the src of the image where you want to display it
+    fr.onload = function (e) {
+        target.src = this.result;
+    };
+    src.addEventListener("change", function () {
+        // fill fr with image data
+        fr.readAsDataURL(src.files[0]);
+    });
+}
--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,72 @@
-// feedback script
-var button = document.getElementById('feedbackButton')
+//просто скриптик - просто
+window.addEventListener('load', function () {
+    function updateOnlineStatus(event) {
+        if (isOnline()) {
+            readOfflineComments();
+        }
+    }
+
+    window.addEventListener('online', updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+});
+
+var i = 0;
+
+function isOnline() {
+    return window.navigator.onLine;
+}
+
+function addComments() {
+    if ($('#name').val() === "" || $('#text').val() === "") {
+        alert('Заповніть всі поля');
+        return false;
+    }
+    if (isOnline()) {
+        var date = new Date;
+        var author = document.getElementById('name').value;
+        var text = document.getElementById('text').value;
+        var parentElem = document.getElementById('reviews-list');
+        var out = document.createElement('div');
+        out.innerHTML =
+            "<div class='container card'><br>" +
+            "   <h2>" + author + "</h2>" +
+            "   <span>" + date + "</span>" +
+            "   <p> <br>" + text + "</p></div>";
+        parentElem.appendChild(out);
+        document.getElementById('form').reset();
+    } else {
+            var date = new Date;
+            var author = document.getElementById('name').value;
+            var text = document.getElementById('text').value;
+            i++;
+            var list = [];
+            list.push({"name": author, "text": text, "date": date});
+            localStorage.setItem('r' + i, JSON.stringify(list));
+        document.getElementById('form').reset();
+    }
+}
+
+function readOfflineComments() {
+        len = localStorage.length + 1;
+        for (var k = 1; k < len; k++) {
+            review = JSON.parse(localStorage.getItem('r' + k));
+            var parentElem = document.getElementById('reviews-list');
+            var out = document.createElement('div');
+            out.id = 'review';
+            out.innerHTML =
+                "<div class='container card'><br>" +
+                "   <span class='review-author'>" + review[0].name + "</span>" +
+                "   <span class='review-date'>" + review[0].date + "</span>" +
+                "   <p><br>" + review[0].text + "</p><br></div>";
+            parentElem.appendChild(out);
+            localStorage.removeItem(k);
+        }
+}
+
+
+
+
+/*var button = document.getElementById('feedbackButton')
 var feedbackField = document.getElementById('feedback')
 var feedbackName = document.getElementById('feedbackName')
 
@@ -36,6 +103,4 @@ button.addEventListener('click', function() {
         feedbackName.value = ''
       }
     }
- });
-
- 
+ });*/
